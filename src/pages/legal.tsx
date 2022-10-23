6689a7f9fa2b244a0a8070655ed02777734aff0c
Paul Zakin <paul@paulzakin.com>
2022-10-23 10:24:22 -0500

    Update README.md

--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -13,12 +13,12 @@ Let's keep this short and sweet ❤️
 - If you use Clean Slate, you agree to the Terms of Use and Privacy Policy
 - To use Clean Slate, you need to be 18 or older. 
 - If we make a significant change to this policy, we will let you know via email.
-- If you want to contact us about something, such as the Terms of Use or Privacy Policy, email ${getContactEmail()}.
+- If you want to contact us about something, such as this Terms of Use or Privacy Policy, email ${getContactEmail()}.
 
 ## Terms of Use
 
 - You cannot use Clean Slate in a way that infringes upon the rights of others, or in any way that is illegal, threatening, fraudulent, or harmful. 
-- We reserve the right to refuse service, terminate accounts, remove content, or cancel orders in our sole discretion.
+- We reserve the right to refuse service and terminate accounts on this hosted service at our sole discretion.
 - Clean Slate is intended for informational purposes only. That means a couple of things. One, we are not a substitute for a doctor. Two, some of our information, like the calorie count of a given food, may be wrong, despite our best effort to ensure accuracy. 
 
 ## Privacy
@@ -27,7 +27,7 @@ Let's keep this short and sweet ❤️
 
   - The data you explicitly provide. That data is stored on the server that serves Clean Slate. For example, app.cleanslate.sh is hosted on [Render.com](https://render.com/privacy). If this instance of Clean Slate is self-hosted, in may be in a different location. Email your host at ${getContactEmail()} for details.
   - The data needed to handle bugs. That data is stored in [Honeybadger](https://www.honeybadger.io/privacy/).
-  - The data needed to securely authenticate you. That data is stored in [Firebase](https://firebase.google.com/support/privacy) by Google.
+  - The data needed to securely authenticate you. That data is stored in [Firebase](https://firebase.google.com/support/privacy), which is owned by Google.
 
 - If you have any questions about your data, or wish to download it, email ${getContactEmail()}.
 `
