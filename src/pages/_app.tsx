b864cf20e9318c3c216c19860039130fac3d1812
Paul Zakin <paul@paulzakin.com>
2022-10-23 12:40:51 -0500

    Bug: Add react-div-100vh to fix viewport error on Mobile Safari

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import '../../node_modules/chagall/src/chagall.scss'
 import 'firebase/compat/auth'
-import { css } from '@emotion/react'
 import { Honeybadger, HoneybadgerErrorBoundary } from '@honeybadger-io/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ReactNode } from 'react'
+import Div100vh from 'react-div-100vh'
 import { StoreContext } from 'storeon/react'
 import { ErrorComponent } from '../components/error/ErrorBoundary'
 import { startHoneybadger } from '../helpers/ui/startHoneybadger'
@@ -27,16 +27,9 @@ function _App({ Component, pageProps }: AppProps) {
           <title>Clean Slate | App</title>
           <meta name="description" content={'Track Food without Judgement'} />
         </Head>
-        <div
-          css={css`
-            min-height: 100vh;
-            > #app {
-              height: 100vh;
-            }
-          `}
-        >
+        <Div100vh>
           <Component {...pageProps} />
-        </div>
+        </Div100vh>
       </StoreContext.Provider>
     </HoneybadgerErrorBoundary>
   )
