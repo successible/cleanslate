96b5a7ddff79c285a68b7fa1c47f1f9a5f132263
Paul Zakin <paul@paulzakin.com>
2022-10-23 16:02:40 -0500

    Bug: Ensure isMobile detects iPads running iOS 13 or greater

--- src/helpers/ui/isMobile.ts
+++ src/helpers/ui/isMobile.ts
@@ -1,6 +1,7 @@
 import { isBrowser } from '../data/isBrowser'
 
 export const isMobile = () => {
+  const UA = navigator.userAgent
   const toMatch = [
     /Android/i,
     /webOS/i,
@@ -10,7 +11,12 @@ export const isMobile = () => {
     /BlackBerry/i,
     /Windows Phone/i,
   ]
-  return toMatch.some((toMatchItem) => {
-    return isBrowser() ? navigator.userAgent.match(toMatchItem) : false
-  })
+  return (
+    // iPad does not show up on the User Agent for Safari (13 or more) on iPads
+    // https://developer.apple.com/forums/thread/119186?page=2
+    (UA.includes('Mac') && 'ontouchend' in document) ||
+    toMatch.some((toMatchItem) => {
+      return isBrowser() ? UA.match(toMatchItem) : false
+    })
+  )
 }
