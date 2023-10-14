15a3e1d384db02ddf982fa5e7d2f56a62660b7f2
Paul <paul@paulzakin.com>
2023-10-13 22:49:32 -0400

    Bug: Use the value of the domain for the API URL

--- src/helpers/getApiUrl.ts
+++ src/helpers/getApiUrl.ts
@@ -1,4 +1,10 @@
+import { isProduction } from './isProduction'
+
 export const getApiUrl = () => {
+  const domain = process.env.NEXT_PUBLIC_HASURA_DOMAIN
+  if (isProduction()) {
+    return [`https://${domain}/v1/graphql`, `wss://${domain}/v1/graphql`]
+  }
   return [
     `https://${window.location.host}/v1/graphql`,
     `wss://${window.location.host}/v1/graphql`,
