84d77a5ac9d26ff07664821d85dc3c5c6b3443bc
Paul <paul@paulzakin.com>
2024-02-04 10:49:15 -0500

    Add firebase-tools as a dev dependency and limit x-hasura-allowed-roles in the Firebase function

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ admin.initializeApp(functions.config().firebase)
 exports.processSignup = functions.auth.user().onCreate((user) => {
   const customClaims = {
     'https://hasura.io/jwt/claims': {
-      'x-hasura-allowed-roles': ['user', 'admin'],
+      'x-hasura-allowed-roles': ['user'],
       'x-hasura-default-role': 'user',
       'x-hasura-user-id': user.uid,
       'x-hasura-username': user.providerData[0].providerId,
