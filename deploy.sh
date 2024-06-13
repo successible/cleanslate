cf79182ad9b851ead7557d8eabd894a14da93748
Paul <paul@paulzakin.com>
2024-06-13 00:17:44 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -33,8 +33,5 @@ docker compose -f $COMPOSE_FILE build
 docker compose -f $COMPOSE_FILE down --remove-orphans -t=0
 docker compose -f $COMPOSE_FILE up -d
 
-if type caddy >/dev/null 2>&1
-then
-    caddy stop
-    caddy start
-fi
+sudo caddy stop
+sudo caddy start
