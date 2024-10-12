bedb3501d76dd57a6064645c7454bb2d554c5c3b
Paul Zakin <paul@paulzakin.com>
2024-10-12 17:55:04 -0400

    Add support for GitHub Packages (#193)

--- deploy.sh
+++ deploy.sh
@@ -5,44 +5,14 @@ git pull origin main
 
 # Set the environmental variables
 export $(xargs < .env)
-
-# Check that HASURA_GRAPHQL_JWT_SECRET has been set in the .env
-if [ -z ${HASURA_GRAPHQL_JWT_SECRET+x} ];
-then
-    echo "You need to define the HASURA_GRAPHQL_JWT_SECRET in your .env. Consult Step #2 in the How do I host Clean Slate? section of the README.md"
-    exit 1
-else
-    echo ""
-fi
-
-# Set the rest of the environmental variables
 export NEXT_PUBLIC_VERSION=$(git rev-parse --short HEAD)
-if [ "$NEXT_PUBLIC_USE_FIREBASE" != "true" ]; then
-    export NEXT_PUBLIC_FIREBASE_CONFIG='{}'
-    export NEXT_PUBLIC_LOGIN_WITH_APPLE='no'
-    export NEXT_PUBLIC_LOGIN_WITH_FACEBOOK='no'
-    export NEXT_PUBLIC_LOGIN_WITH_GITHUB='no'
-    export NEXT_PUBLIC_LOGIN_WITH_GOOGLE='no'
-    export NEXT_PUBLIC_REACT_SENTRY_DSN=''
-    export NEXT_PUBLIC_USE_FIREBASE='false'
-fi
 
-if [ "$NEXT_PUBLIC_USE_FIREBASE" == "true" ]; then
-    # This value is unused by Firebase, but it silences the Docker Compose warning
-    export JWT_SIGNING_SECRET=$(uuidgen)
-fi
-
-
-if [ -n "$COMPOSE_FILE" ]; then
-  echo "Deploying with file: $COMPOSE_FILE"
-else
-  COMPOSE_FILE=docker-compose.yml
-  echo "Deploying with file: $COMPOSE_FILE"
-fi
-
+# Build the containers and run them with Docker Compose
+COMPOSE_FILE=docker-compose.yml
 docker compose -f $COMPOSE_FILE build
 docker compose -f $COMPOSE_FILE down --remove-orphans -t=0
 docker compose -f $COMPOSE_FILE up -d
 
+# Start the reverse proxy, assuming the proxy is caddy
 sudo caddy stop
 sudo caddy start
--- src/helpers/getApiUrl.ts
+++ src/helpers/getApiUrl.ts
@@ -1,9 +1,6 @@
-import { isProduction } from './isProduction'
+import { isBrowser } from './isBrowser'
 
 export const getApiUrl = () => {
-  const domain = process.env.NEXT_PUBLIC_HASURA_DOMAIN
-  if (isProduction()) {
-    return [`https://${domain}/v1/graphql`, `wss://${domain}/v1/graphql`]
-  }
-  return ['https://localhost/v1/graphql', 'wss://localhost/v1/graphql']
+  const domain = isBrowser() ? window.location.hostname : ''
+  return [`https://${domain}/v1/graphql`, `wss://${domain}/v1/graphql`]
 }
