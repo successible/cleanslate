78a6ffa273fb2d9034004ba780740a3375629cc8
Paul <paul@paulzakin.com>
2024-10-12 18:47:28 -0400

    Update tag.yml

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ fi
 
 # Build the containers and run them with Docker Compose
 COMPOSE_FILE=docker-compose.yml
-docker compose -f $COMPOSE_FILE build
 docker compose -f $COMPOSE_FILE down --remove-orphans -t=0
 docker compose -f $COMPOSE_FILE up -d
 
