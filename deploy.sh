010ca4e82fa37891b92efe09e1337c6edcac42e5
Paul <paul@paulzakin.com>
2024-10-12 18:33:18 -0400

    Update tag.yml

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ git pull origin main
 # Set the environmental variables
 export $(xargs < .env)
 export NEXT_PUBLIC_VERSION=$(git rev-parse --short HEAD)
+if [ "$NEXT_PUBLIC_USE_FIREBASE" == "true" ]; then
+    export PULL_POLICY="build"
+fi
 
 # Build the containers and run them with Docker Compose
 COMPOSE_FILE=docker-compose.yml
