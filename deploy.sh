77ea320177f5be72a81f4d000b4932bf4fb0cb81
Paul <paul@paulzakin.com>
2024-10-12 23:34:05 -0400

    Only set PULL_POLICY when NEXT_PUBLIC_USE_FIREBASE is true

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ git pull origin main
 # Set the environmental variables
 export $(xargs < .env)
 export NEXT_PUBLIC_VERSION=$(git rev-parse --short HEAD)
-if [ "$NEXT_PUBLIC_USE_FIREBASE" == "true" ]; then
-    export PULL_POLICY="build"
-fi
-export PULL_POLICY="build"
 
 # Build the containers and run them with Docker Compose
 COMPOSE_FILE=docker-compose.yml
+if [ "$NEXT_PUBLIC_USE_FIREBASE" == "true" ]; then
+    export PULL_POLICY="build"
+    docker compose -f $COMPOSE_FILE build
+fi
 docker compose -f $COMPOSE_FILE down --remove-orphans -t=0
 docker compose -f $COMPOSE_FILE up -d
 
