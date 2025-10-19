0cb88f22f87782bc374b838ff77154296dbd6ccf
Paul <paul@paulzakin.com>
2025-10-19 01:01:50 -0400

    Reduce rounding

--- src/components/forms/ExerciseForm/helpers/calculateCaloriesFromMETs.test.ts
+++ src/components/forms/ExerciseForm/helpers/calculateCaloriesFromMETs.test.ts
@@ -3,4 +3,5 @@ import { calculateCaloriesFromMETs } from './calculateCaloriesFromMETs'
 
 test('calculateCaloriesFromMETs works as expected', () => {
   expect(calculateCaloriesFromMETs(200, 60, 3)).toBe(286)
+  expect(calculateCaloriesFromMETs(200, 60, 10)).toBe(953)
 })
