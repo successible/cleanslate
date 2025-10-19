4c5571e0aeb6d0fc93cbdac9c0f44096b954e23f
Paul Zakin <paul@paulzakin.com>
2025-10-19 01:25:59 -0400

    Ensure the MET formulas use numbers for keys, not strings (#375)

--- src/components/forms/ExerciseForm/helpers/getRowingMET.ts
+++ src/components/forms/ExerciseForm/helpers/getRowingMET.ts
@@ -3,8 +3,10 @@ import rowingMET from '../data/MET/rowingMET.json'
 
 export const getRowingMET = (watts: number) => {
   // Formula: y = 0.048x + 1.6, r = 0.985
-  const MET: Record<string, number> = rowingMET
-  const exactMET = MET[String(watts)]
+  const MET: Record<number, number> = Object.fromEntries(
+    Object.entries(rowingMET).map(([k, v]) => [Number(k), v])
+  )
+  const exactMET = MET[watts]
   // If we have an exact MET match for watts, use that.
   // Otherwise, return the predicated MET
   return exactMET || round(0.048 * watts + 1.6, 2)
--- src/components/forms/ExerciseForm/helpers/getRunningMET.ts
+++ src/components/forms/ExerciseForm/helpers/getRunningMET.ts
@@ -3,8 +3,10 @@ import runningMETs from '../data/MET/runningMET.json'
 
 export const getRunningMET = (mph: number) => {
   // Formula: y = 1.6094x + 0.3157, r = 0.99
-  const MET: Record<string, number> = runningMETs
-  const exactMET = MET[String(mph)]
+  const MET: Record<number, number> = Object.fromEntries(
+    Object.entries(runningMETs).map(([k, v]) => [Number(k), v])
+  )
+  const exactMET = MET[mph]
   // If we have an exact MET match for MPH, use that.
   // Otherwise, return the predicated MET
   return exactMET || round(1.6094 * mph + 0.3157, 2)
