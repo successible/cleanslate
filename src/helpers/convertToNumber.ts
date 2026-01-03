1e2ceb7237ed74e6efdd6fc1615aa8bd65ac7cdf
Paul Zakin <paul@paulzakin.com>
2026-01-03 15:51:00 -0500

    Handle locales in which a comma is used instead of a period (#426)

--- src/helpers/convertToNumber.ts
+++ src/helpers/convertToNumber.ts
@@ -2,7 +2,14 @@ export const convertToNumber = (amount: string | number): number | null => {
   if (amount === '') {
     return null
   }
-  const value = Number(amount)
+
+  let amountToUse = amount
+  // Handle locales in which a comma is used instead of a period
+  if (typeof amountToUse === 'string') {
+    amountToUse = amountToUse.replaceAll(',', '.')
+  }
+
+  const value = Number(amountToUse)
   if (Number.isNaN(value) || value === Number.POSITIVE_INFINITY) {
     return null
   }
