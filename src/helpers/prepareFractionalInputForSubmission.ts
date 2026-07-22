81eff1ce99854fb6241b8599ab6b487cf147f991
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2026-07-22 00:22:44 -0400

    Evaluate numeric string input expressions (#507)

--- src/helpers/prepareFractionalInputForSubmission.ts
+++
@@ -1,8 +0,0 @@
-import { convertToNumber } from './convertToNumber'
-
-/**
- * To avoid any GraphQL errors, convert any fraction inputs to numbers and any "" to null
- */
-export const prep = (input: string | number): number | null => {
-  return convertToNumber(input)
-}
